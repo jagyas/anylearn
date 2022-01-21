bb943459c59fd6acead859125407fb6d64494641
Jagdeep Singh <jagdeep@anylearn.io>
2022-01-21 13:15:44 +0530

    Update index.js

--- packages/webapp/pages/index.js
+++ packages/webapp/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 export default function Page () {
   return (
     <Layout>
-      <h1>NextAuth.js Example 3</h1>
+      <h1>NextAuth.js Example 2</h1>
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
