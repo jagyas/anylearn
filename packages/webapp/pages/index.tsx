8514e615a6bea261c0969f3c8e634719519a6908
Jagdeep Singh <jagdeep@anylearn.io>
2022-07-15 09:02:56 +0530

    Update index.tsx

--- packages/webapp/pages/index.tsx
+++ packages/webapp/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          1 Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
